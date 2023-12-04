fc82ed58ee60f2de242d26b9a8462d5d0448ae2d
Dağhan Sever <daghansever@gmail.com>
2023-12-04 21:08:51 +0300

    Update TermProject.cpp

--- TermProject.cpp
+++ TermProject.cpp
@@ -3,6 +3,18 @@
 #include <iostream>
 using namespace std;
 
+//Task1: You must implement a hash table as your main data structure.You should implement your own hash table, 
+//you cannot use a hash table implementation from a library.
+//This implementation should be a proper C++ class.
+//You should use this hash table for storing the filenames and the corresponding number of visits of that page(filename).
+//There is no restriction on the choice of the hash function and collision resolution method.
+//
+//Task2 : Use std::unordered_map data structure for storing the filenames and the corresponding number of visits.
+//Task 3 : In order to compare the efficiency of your own hash table implementation and std::unordered_map, write a program which measures the total time it takes starting from reading access_log file until the end of printing the top 10 most visited pages.There is no need to ask user input.
+//Note1 : For finding the top 10 most visited pages try to find an efficient method.Hint : you can use the heap data structures.
+//Note2 : Other than the hash table implementation, you can use C++ standard library for your needs.
+
+
 int main()
 {
     std::cout << "Hello World!\n";
